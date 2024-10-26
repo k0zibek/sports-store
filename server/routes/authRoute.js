4eb317155129dfc87388958e17becca7d2bea039
Kazi <uidboy01@gmail.com>
2024-10-26 08:33:24 +0500

    changed router & added user model

---
+++ server/routes/authRoute.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+// router object
+const router = express.Router();
+
+// routing
+// REGISTER || METHOD POST
+router.post('/register', registerController);
+
+export default router;
