a554e02f46b45289ed17c25a6fc8de29d3bae925
Kazi <uidboy01@gmail.com>
2024-10-26 12:55:57 +0500

    basic pages and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
-      <h1>It works!</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-3">
+      <h4 className="text-center">All rights Reserved &copy; Kaz1</h4>
+    </div>
+  );
+};
+
+export default Footer;
