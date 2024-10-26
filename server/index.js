91d6065f0a9ead3ffd30612d8b5278f1ea82cf09
Kazi <uidboy01@gmail.com>
2024-10-26 08:24:49 +0500

    db connection

--- server/index.js
+++ server/index.js
@@ -1,24 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const productRoutes = require('./routes/productRoutes');
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import cors from 'cors';
+import colors from 'colors';
+import router from './routes/productRoutes.js';
+import connectDB from './config/db.js';
 
+// configure env
 dotenv.config();
+const PORT = process.env.PORT;
+const DEV_MODE = process.env.DEV_MODE;
 
+// database config
+connectDB();
+
+// REST object
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use('/api/products', productRoutes);
-
-app.use(cors());
+// middlewares
 app.use(express.json());
+app.use(morgan('dev'));
+app.use('/api/products', router);
+app.use(cors());
 
+// REST api
 app.get('/', (req, res) => {
-	res.send('API is working');
+	res.send('<h1>API is working</h1>');
 });
 
-mongoose
-	.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
-	.catch((error) => console.error(error.message));
+// run listen
+app.listen(PORT, () => {
+	console.log(`Server running on ${DEV_MODE} mode on port ${PORT}`.bgCyan.white);
+});
